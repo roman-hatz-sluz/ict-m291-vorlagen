cc58dfb2d7f6fa498bc9ffcfbbbcfd975d04fe4e
roman-hatz-sluz <roman.hatz@sluz.ch>
2023-06-22 10:34:37 +0200

    clean up deps

--- test-student-code.sh
+++ test-student-code.sh
@@ -10,7 +10,7 @@ for folder in "$directory"/*; do
     folder_name=$(basename "$folder")
     echo "process $folder_name ..."
     purifycss=$(./node_modules/.bin/purifycss  -ri $folder/**/*.css $folder/**/*.{html,js}  -o /dev/null)
-    csslint=$(rm -f output.css; find "$folder"  -name '*.css' -type f -exec cat {} \; > output.css; node_modules/.bin/csslint --config=.csslint  --quiet ./output.css)
+    csslint=$(rm -f output.css; find "$folder"  -name '*.css' -type f -exec cat {} \; > output.css; node node_modules/.bin/csslint --config=.csslint  --quiet ./output.css)
     htmlhint=$(node_modules/.bin/htmlhint --config .htmlhint "$folder")
     eslint=$(node_modules/.bin/eslint $folder)
     rm -f ./student-reports/${folder_name}.txt
